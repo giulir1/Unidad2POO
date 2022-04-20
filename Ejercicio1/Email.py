6c5eb10587c64717f47521574dee06ae2414e4bf
giulir1 <83610835+giulir1@users.noreply.github.com>
2022-04-20 20:15:43 -0300

    Create Email.py

---
+++ Ejercicio1/Email.py
@@ -0,0 +1,28 @@
+class Email:
+    __idCuenta = ''
+    __dominio = ''
+    __tipoDominio = ''
+    __contrasena = ''
+
+    def __init__(self, idCuenta, dominio, tipoDominio, contrasena):
+        self.__idCuenta = idCuenta
+        self.__dominio = dominio
+        self.__tipoDominio = tipoDominio
+        self.__contrasena = contrasena
+
+    def retornaEmail(self):
+        return '{}@{}.{}'.format(self.__idCuenta, self.__dominio, self.__tipoDominio)
+
+    def getDominio(self):
+        return self.__dominio
+
+    def crearCuenta(self, mail, contrasena):
+        str1 = mail.split('@', 1)       # divide el email en idCuenta y dominio.tipoDominio
+        str2 = str1[1].split('.', 1)    # divide el dominio y el tipoDominio
+        self.__idCuenta = str1[0]
+        self.__dominio = str2[0]
+        self.__tipoDominio = str2[1]
+        self.__contrasena = contrasena
+
+    def cambiarContrasena(self, nuevaContra):
+        self.__contrasena = nuevaContra
