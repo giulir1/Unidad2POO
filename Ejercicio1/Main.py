86a375b38d1724c5a4eeb763aa65acf6fcdc8991
giulir1 <83610835+giulir1@users.noreply.github.com>
2022-04-22 20:21:52 -0300

    Update Main.py

--- Ejercicio1/Main.py
+++ Ejercicio1/Main.py
@@ -1,26 +1,31 @@
-from Email import Email
-import ManejadorEmail
+from ManejadorEmail import controlEmail
 
 if __name__ == '__main__':
-    print('Ingrese su nombre:')
-    nombre = input()
-    print('Ingrese el ID de la cuenta:')
-    idCuent = input()
-    print('Ingrese el dominio de la cuenta:')
-    dom = input()
-    print('Ingrese el tipo de dominio de la cuenta:')
-    tipoDom = input()
-    print('Ingrese la contraseña de la cuenta:')
-    contra = input()
+    opcion1 = 5
+    control = controlEmail()
+    while opcion1 != 0:
+        print('Elija una acción:\n1--Crear cuenta\n2--Modificar contraseña\n3--Crear instancia de la clase\n4--Verificar email\n0--SALIR')
+        opcion1 = int(input())
 
-#   INCISO 1
-    unEmail = Email(idCuent, dom, tipoDom, contra)
-    print('Estimado {}, te enviaremos tus mensajes a la dirección {}'.format(nombre, unEmail.retornaEmail()))
+        if opcion1 == 1:
+            control.punto1()
+            print('¿Desea cambiar la contraseña?\1--SI\n2--NO')
+            opcion2 = int(input())
+            if opcion2 == 1:
+                control.punto2()
+                print('\n')
+                input('Presione cualquier tecla para continuar.')
+            else:
+                print('No se modificará la contraseña\n')
+                input('Presione cualquier tecla para continuar.')
+"""        if opcion1 == 2:
 
-#   INCISO 2
+
+   INCISO 2
     print('\n\n¿Desea cambiar la contraseña?\n0--SI\n1--NO')
     op = input()
     if op == '0':
         print('Se cambiará la contraseña.')
     else:
         print('No se cambiará la contraseña.')
+"""
