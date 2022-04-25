5265192a0beb7c6e8c5ebd183b0e09ceed3e9389
giulir1 <83610835+giulir1@users.noreply.github.com>
2022-04-25 11:39:02 -0300

    Update ManejadorEmail.py

--- Ejercicio1/ManejadorEmail.py
+++ Ejercicio1/ManejadorEmail.py
@@ -18,15 +18,31 @@ class controlEmail:
         tipoDom = input()
         print('Ingrese la contraseña de la cuenta:')
         contra = input()
-        unEmail = Email(idCuent, dom, tipoDom, contra)
+        unEmail = Email(idCuent, dom, tipoDom, contra)  # Crea la instancia de la clase Email
+# Arma el email con los atributos ingresados
         print('Estimado {}, te enviaremos tus mensajes a la dirección {}'.format(nombre, unEmail.retornaEmail()))
-        self.__lista.append(unEmail)
+        self.__lista.append(unEmail)                    # Agrega el objeto a la lista
 
     def punto2(self, mail):
-        pass
-
-"""
-    def leerlista(self):
         for i in self.__lista:
+            if mail == i:
+                print('Ingrese la contraseña actual: ')
+                contraActual = input()
+                while contraActual != i.getContrasena():
+                    print('Contraseña incorrecta. Intente otra vez.')
+                    contraActual = input()
+                print('Ingrese la nueva contraseña: ')
+                contraNueva = input()
+                i.cambiarContrasena(contraNueva)           # Cambia la contraseña del email
+
+    def getMail(self, email):
+        for i in self.__lista:
+            while i.retornaEmail() != email:
+                print('ERROR. Email incorrecto. Intente nuevamente:')
+                email = input()
+            return i                                        # Devuelve el objeto de la clase
+
+    def leerlista(self):
+        for i in self.__lista:                              # Recorre la lista y muestra mail y contraseña
             print(i.retornaEmail())
-"""
+            print('Contraseña: {}\n'.format(i.getContrasena()))
